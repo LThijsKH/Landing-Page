8b2c41eb357df4d8e4437e1bba6001c8b5f8498d
Leo <leokh0713@gmail.com>
2025-11-29 10:49:53 -0500

    feat: switching to frozen flask

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+
+@app.route("/")
+def landing():
+    return render_template("index.html")
+
+@app.route("/photography")
+def photography():
+    return render_template("photography.html")
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+# Setup that uses frozen flask to generate static pages in /build for cloudflare pages
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()  # This will generate static files in a build/ directory
