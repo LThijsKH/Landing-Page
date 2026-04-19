284a8a04c310e2c50640a2eee4f370e9c9652aed
Leo <leokh0713@gmail.com>
2026-04-19 18:36:04 -0400

    chore: Clean old traditional css

--- app.py
+++ app.py
@@ -17,11 +17,9 @@ def landing():
 @app.route("/photography/")
 def photography():
     with open("static/data/photos.json") as f:
-        images_list = json.load(f)
-    columns = split_list(images_list)
-    columns = [columns[0], columns[2], columns[1]] # col2 last of lists which is the longest
-    images = columns[0] + columns[1] + columns[2]
-    return render_template("photography.html", col1=columns[0], col2=columns[1], col3=columns[2], images=images, total_i=len(images))
+        images = json.load(f)
+    col1, col2, col3 = split_list(images)
+    return render_template("photography.html", images=images, col1=col1, col2=col2, col3=col3, total_i=len(images))
 @app.route('/projects/')
 def projects():
     with open("static/data/projects.json") as f:
